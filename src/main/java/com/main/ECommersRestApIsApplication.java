96ce563aaf8e2831d300d26a41aac688a4eae574
DNP DEVELOPER <44169383+dnp176@users.noreply.github.com>
2025-06-07 10:01:47 +0530

    Update ECommersRestApIsApplication.java

--- src/main/java/com/main/ECommersRestApIsApplication.java
+++ src/main/java/com/main/ECommersRestApIsApplication.java
@@ -1,13 +1,32 @@
-package com.main;
+// package com.main;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class ECommersRestApIsApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(ECommersRestApIsApplication.class, args);
+// 	}
+
+// }
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ECommersRestApIsApplication {
+public class ECommersRestApisApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommersRestApIsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommersRestApisApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ECommersRestApisApplication.class);
+    }
 }
