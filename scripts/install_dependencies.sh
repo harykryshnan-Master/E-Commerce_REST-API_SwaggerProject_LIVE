6caee1e18bd9a788bd7e72d81282f0c0a8c2dfe2
DNP DEVELOPER <44169383+dnp176@users.noreply.github.com>
2025-06-07 19:43:23 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+echo "=== Installing required packages ==="
+apt-get update -y
+apt-get install -y openjdk-17-jdk unzip tomcat10
+
+echo "=== Creating deployment directory if not exists ==="
+mkdir -p /home/ubuntu/deployments/
+
+echo "=== Cleaning old WAR files ==="
+rm -rf /home/ubuntu/deployments/ROOT.war
+
+echo "Dependencies installed and old WAR removed."
 
